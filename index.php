0bb963a661121ee68de798cca92d307fe10cc493
Jay <96941158+Joubran@users.noreply.github.com>
2026-06-15 17:32:13 +1000

    asd

--- index.php
+++ index.php
@@ -4,15 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Alboni Joubran</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0
-    /dist/css/bootstrap.min.css" rel="stylesheet" integrity
-    ="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnu
-    Z2cdeRhO02iuK6" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/boo
-    tstrap@5.3.0/dist/js/bootstrap.bundle.min.js" 
-    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39W
-    m4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" 
-    crossorigin="anonymous"></script>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -20,11 +16,10 @@
         <div class="row">
             <div class="col-12 text-center">
                 <h1 class="mb-4">Login!</h1>
+
                 <div class="d-flex justify-content-center gap-3">
-                    <a href="/registeration.html" class="btn 
-                    btn-primary">Register</a>
-                    <a href="/login.html" class="btn 
-                    btn-primary">Login</a>
+                    <a href="registeration.html" class="btn btn-primary">Register</a>
+                    <a href="login.html" class="btn btn-primary">Login</a>
                 </div>
             </div>
         </div>
