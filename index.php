ddab73973b85023dfc7b3e4106427aed7616bc3e
Jay <96941158+Joubran@users.noreply.github.com>
2026-06-15 17:47:17 +1000

    asdasds

--- index.php
+++ index.php
@@ -4,22 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Alboni Joubran</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="css/style.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="container d-flex justify-content-center align-items-center vh-100">
         <div class="row">
             <div class="col-12 text-center">
-                <h1 class="mb-4">Registration</h1>
-                <form action="/registration.html" method="POST" class="d-flex flex-column gap-3">
-                    <input type="text" name="login" class="form-control-hacker-input" placeholder="login">
-                    <input type="email" name="email" class="form-control-hacker-input" placeholder="email">
-                    <input type="password" name="password" class="form-control-hacker-input" placeholder="password">
-                    <button class="btn btn-primary" type="submit" name="submit">Register</button>
-                    <p class="mt-3">Alredy have an account?<a href="/login.html">Login</a></p>
-                </form>
+                <h1 class="mb-4">Login!</h1>
+
+                <div class="d-flex justify-content-center gap-3">
+                    <a href="registeration.html" class="btn btn-primary">Register</a>
+                    <a href="login.html" class="btn btn-primary">Login</a>
+                </div>
             </div>
         </div>
     </div>
