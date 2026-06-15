de1063469aead15ef32ef7c27cba71640b430fa0
Jay <96941158+Joubran@users.noreply.github.com>
2026-06-15 22:04:38 +1000

    asdasd

--- posts.php
+++ posts.php
@@ -1,5 +1,19 @@
+<?php
+$link = mysqli_connect('127.0.0.1', 'root', 'kali', 'first');
+
+$id = $_GET['id'];
+
+$sql = "SELECT * FROM posts WHERE id=$id";
+$res = mysqli_query($link, $sql);
+
+$rows = mysqli_fetch_array($res);
+
+$title = $rows['title'];
+$main_text = $rows['main_text'];
+?>
+
 <!DOCTYPE html>
-<html lang="en"> 
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,10 +26,12 @@
     <div class="container d-flex justify-content-center align-items-center vh-100">
         <div class="row">
             <div class="col-12 text-center">
-
+                <?php
+                    echo "<h1>$title</h1>";
+                    echo "<p>$main_text</p>";
+                ?>
             </div>
         </div>
     </div>
 </body>
 </html>
-//done
