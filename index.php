e8187e4f07a21f17fc7f6572c0373d8da918f3bd
Jay <96941158+Joubran@users.noreply.github.com>
2026-06-15 18:22:54 +1000

    asdasdaasd

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <div class="container d-flex justify-content-center align-items-center vh-100">
