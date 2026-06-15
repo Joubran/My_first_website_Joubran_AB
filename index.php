26f8088c254b0a2124e23b6cbe5ba32796b76a77
Joubran Alboni <Joubran.alboni2@gmail.com>
2026-06-15 10:25:13 +0300

    1

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alboni Joubran</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0
+    /dist/css/bootstrap.min.css" rel="stylesheet" integrity
+    ="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnu
+    Z2cdeRhO02iuK6" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/boo
+    tstrap@5.3.0/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39W
+    m4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" 
+    crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container d-flex justify-content-center align-items-center vh-100">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1 class="mb-4">Login!</h1>
+                <div class="d-flex justify-content-center gap-3">
+                    <a href="/registeration.html" class="btn 
+                    btn-primary">Register</a>
+                    <a href="/login.html" class="btn 
+                    btn-primary">Login</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
